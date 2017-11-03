ed29e741bad0654909a0a3b0f28c98a79692cddc
herocrx <hubertkuc13@gmail.com>
2017-11-03 12:34:31 +0100

    W is already implemented, now you have to implement other letters

--- Window.cpp
+++ Window.cpp
@@ -14,25 +14,30 @@ GLuint indexBufferID;
 
 void Window::paintGL() {
     glViewport(0,0,width(),height());
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+  //  glDrawArrays(GL_LINE_SMOOTH, 0, 6);
+
+    glDrawArrays(GL_LINE_STRIP,0,5);
 
 
 }
 
 void sendDataToOpenGL()
 {
+
     GLfloat verts [] =
-            {
-                    +0.0f, +1.0f,
-                    +1.0f, +0.0f, +0.0f,
-                    -1.0f, -1.0f,
-                    +0.0f, +1.0f, +0.0f,
-                    +1.0f, -1.0f,
-                    +0.0f, +0.0f, +1.0f,
+            {    -1.0f, 0.5f, 0.0f,
+                 0.0f,  0.0f,1.0f,
+                -0.9f, -0.5f, 0.0f,
+                1.0f,  0.0f,0.0f,
+                -0.8f, 0.5f, 0.0f,
+                0.0f,  0.0f,1.0f,
+                -0.7f, -0.5f, 0.0f,
+                1.0f,  0.0f,0.0f,
+                -0.6f, 0.5f, 0.0f,
+                0.0f,  0.0f,1.0f,
 
             };
     GLuint triangleBufferID;
-
 
     glGenVertexArrays(1,&triangleBufferID);
     glBindVertexArray(triangleBufferID);
@@ -41,19 +46,20 @@ void sendDataToOpenGL()
     glBindBuffer(GL_ARRAY_BUFFER,triangleBufferID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), &verts, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char *) (sizeof(float) * 2));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char *) (sizeof(float) * 3));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-    //glDrawElements(GL_TRIANGLES,3,GL_UNSIGNED_SHORT,&indexBufferID );
+    glLineWidth(8.0);
+    //glDrawArrays(GL_POINTS,0,3);
 
-
+/*
     GLushort indices[] = { 0 , 1 , 2};
     glGenBuffers(1, &indexBufferID);
     glGenVertexArrays(1,&indexBufferID);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,indexBufferID);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
-
+*/
 
 }
 
