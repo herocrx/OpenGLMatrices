2bb0aaac9ba81e6296fc441bbc376dd9d190ffd6
herocrx <hubertkuc13@gmail.com>
2017-11-05 12:08:39 +0100

    Multiple objects, Write Werka works

--- Window.cpp
+++ Window.cpp
@@ -10,141 +10,188 @@ extern const char * vertexShaderCode;
 extern const char * fragmentShaderCode;
 
 
-GLuint indexBufferID;
-
 void Window::paintGL() {
-    glViewport(0,0,width(),height());
-    glDrawArrays(GL_LINES,0,40);
-
 
 }
 
 void sendDataToOpenGL()
 {
 
-    GLfloat verts [] =
+    GLfloat vertsW[] =
             {
-                    -1.0f, +0.5f, +0.0f,
-                 +0.0f, +0.0f, +1.0f,
-                 -0.9f, -0.5f, +0.0f,
-                 +1.0f, +0.0f, +0.0f,
-
-                 -0.9f, -0.5f, 0.0f,
-                 1.0f,  0.0f,0.0f,
-                 -0.8f, 0.5f, 0.0f,
-                 0.0f,  0.0f,1.0f,
-
-                 -0.7f, -0.5f, 0.0f,
-                 1.0f,  0.0f,0.0f,
-                -0.8f, 0.5f, 0.0f,
-                0.0f,  0.0f,1.0f,
+                -0.8f, +0.5f, +0.0f,
+                +0.0f, +0.0f, +1.0f,
+                -0.7f, -0.5f, +0.0f,
+                +1.0f, +0.0f, +0.0f,
 
                 -0.7f, -0.5f, 0.0f,
                 1.0f,  0.0f,0.0f,
                 -0.6f, 0.5f, 0.0f,
                 0.0f,  0.0f,1.0f,
 
-                 -0.5f, 0.5f, 0.0f,
-                 0.0f,  0.0f,1.0f,
-                 -0.5f, -0.5f, 0.0f,
-                 1.0f,  0.0f,0.0f,
+                -0.5f, -0.5f, 0.0f,
+                1.0f,  0.0f,0.0f,
+                -0.6f, 0.5f, 0.0f,
+                0.0f,  0.0f,1.0f,
 
-                 -0.513f, 0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-                 -0.30f, 0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
+                -0.5f, -0.5f, 0.0f,
+                1.0f,  0.0f,0.0f,
+                -0.4f, 0.5f, 0.0f,
+                0.0f,  0.0f,1.0f,
 
+            };
 
-                 -0.513f, 0.0f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-                 -0.30f, 0.0f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
+    GLfloat vertwE[]=
+            {
+                    -0.3f, +0.5f, +0.0f,
+                    +0.0f, +0.0f, +1.0f,
+                    -0.3f, -0.5f, +0.0f,
+                    +0.0f, +0.0f, +1.0f,
 
-                 -0.513f, -0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-                 -0.30f, -0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-
-                 -0.20f, 0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-                 -0.20f, -0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-
-                 -0.20f, 0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-                 -0.0f, 0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-
-                 0.0f, 0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.0f, -0.05f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-
-                 0.0f, 0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.0f, -0.02f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-
-                 -0.2f,  0.0f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.0f,  0.0f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-
-                 -0.2f,  0.0f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.0f,  -0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-
-                 0.2f,  0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.2f,  -0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-
-                 0.2f,  0.0f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.4f,  0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-
-                 0.2f,  0.0f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.4f,  -0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
+                    -0.313f,+0.5f, +0.0f,
+                    +0.0f,  +0.0f, +1.0f,
+                    -0.10f, +0.5f, +0.0f,
+                    +1.0f,  +0.0f, +0.0f,
 
 
-                 0.5f,  -0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-                 0.6f,  0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
+                    -0.313f, 0.0f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+                    -0.10f, 0.0f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
 
-                 0.6f,  0.5f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-                 0.7f,  -0.5f, 0.0f,
-                 1.0f,  0.0f, 0.0f,
-
-                 0.55f,  0.0f, 0.0f,
-                 1.0f,  0.0f, 1.0f,
-                 0.65f,  -0.0f, 0.0f,
-                 0.0f,  0.0f, 1.0f,
-
-
+                    -0.313f, -0.5f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+                    -0.10f, -0.5f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
 
 
             };
-    GLuint triangleBufferID;
 
-    glGenVertexArrays(1,&triangleBufferID);
-    glBindVertexArray(triangleBufferID);
+       GLfloat vertsR[] =
+            {
+                    -0.0f, 0.5f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+                    -0.0f, -0.5f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
 
-    glGenBuffers(1, &triangleBufferID);
-    glBindBuffer(GL_ARRAY_BUFFER,triangleBufferID);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), &verts, GL_STATIC_DRAW);
+                    -0.0f, 0.5f, 0.0f,
+                    0.0f, 0.0f, 1.0f,
+                    0.2f, 0.5f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
+
+                    0.2f, 0.5f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
+                    0.2f, -0.05f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+
+                    0.0f, 0.0f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
+                    0.2f, -0.5f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+
+                    0.0f,  0.0f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
+                    0.2f,  -0.0f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+
+            };
+
+       GLfloat vertsK[]=
+            {
+                    0.4f,  0.5f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
+                    0.4f,  -0.5f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+
+                    0.4f,  0.0f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
+                    0.6f,  0.5f, 0.0f,
+                    0.0f,  0.0f, 1.0f,
+
+                    0.4f,  0.0f, 0.0f,
+                    1.0f,  0.0f, 0.0f,
+                    0.6f,  -0.5f, 0.0f,
+                    0.0f,  0.0f, 1.0f
+            };
+
+             GLfloat vertsA[] =
+            {
+                    0.7f,  -0.5f, 0.0f,
+                 1.0f,  0.0f, 0.0f,
+                 0.8f,  0.5f, 0.0f,
+                 0.0f,  0.0f, 1.0f,
+
+                 0.8f,  0.5f, 0.0f,
+                 0.0f,  0.0f, 1.0f,
+                 0.9f,  -0.5f, 0.0f,
+                 1.0f,  0.0f, 0.0f,
+
+                 0.75f,  0.0f, 0.0f,
+                 1.0f,  0.0f, 1.0f,
+                 0.85f,  -0.0f, 0.0f,
+                 0.0f,  0.0f, 1.0f,
+            };
+
+
+
+    const int numberBuffers = 2;
+
+    GLuint triangleBufferID0[numberBuffers];
+    GLuint triangleBufferID1;
+
+
+    glGenBuffers(4, triangleBufferID0);
+    glGenBuffers(1, &triangleBufferID1);
+    std::cout << "First buffer: " << triangleBufferID0[0] << std::endl;
+    std::cout << "Second buffer: " <<  triangleBufferID0[1] << std::endl;
+    glBindBuffer(GL_ARRAY_BUFFER,triangleBufferID0[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*48 , vertsW, GL_STATIC_DRAW);
+
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char *) (sizeof(float) * 3));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-    glLineWidth(8.0);
-    //glDrawArrays(GL_POINTS,0,3);
+
+    glDrawArrays(GL_LINES,0,24);
+
+    glBindBuffer(GL_ARRAY_BUFFER,triangleBufferID0[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*60 , vertsR, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char *) (sizeof(float) * 3));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glLineWidth(4.0);
+    glDrawArrays(GL_LINES,0,32);
+
+
+    glBindBuffer(GL_ARRAY_BUFFER,triangleBufferID1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*60, vertwE, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char *) (sizeof(float) * 3));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glDrawArrays(GL_LINES,0,8);
+
+
+    glBindBuffer(GL_ARRAY_BUFFER,triangleBufferID0[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*36, vertsK, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char *) (sizeof(float) * 3));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glDrawArrays(GL_LINES,0,8);
+
+    glLineWidth(2);
+    glBindBuffer(GL_ARRAY_BUFFER,triangleBufferID0[3]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*36, vertsA, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char *) (sizeof(float) * 3));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glDrawArrays(GL_LINES,0,8);
+
+
 
 }
 
@@ -189,6 +236,8 @@ void installShaders()
 void Window::initializeGL() {
     glewInit();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0,0,width(),height());
+
     installShaders();
     sendDataToOpenGL();
 
